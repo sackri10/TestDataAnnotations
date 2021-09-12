b8808b359979d605c92a055b0c7b64510bfa14c0
Datta Sai Krishna <sackri10@gmail.com>
2021-09-12 23:24:27 +0530

    Add project files.

---
+++ Webapi/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webapi.Models;
+
+namespace Webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        public IActionResult Test(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                _logger.LogInformation("Valid");
+
+            }
+            return Ok();
+        }
+    }
+}
---
+++ Webapi/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapi.Models
+{
+    public class User
+    {
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
